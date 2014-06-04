37be3efd4f3bc929ebfd9ac5668f3d2bbcf7b3a9
Ilya Roslyakov <uprial@gmail.com>
2014-06-04 15:53:24 +0400

    Added VISUAL

--- ubuntu-1st/profile.sh
+++ ubuntu-1st/profile.sh
@@ -3,16 +3,17 @@
 sed -i "s/^alias l='ls \-CF'/#alias l='ls -CF'/g" ~/.bashrc
 
 if [[ -z `grep "^alias \.\.=" ~/.bashrc` ]]; then
-	echo "alias ..='cd ..'" >> ~/.bashrc
-	echo "alias l='ls -laG'" >> ~/.bashrc
+    echo "alias ..='cd ..'" >> ~/.bashrc
+    echo "alias l='ls -laG'" >> ~/.bashrc
 fi
 
 if [[ -z `grep "^alias r=" ~/.profile` ]]; then
-	echo "alias r='screen -RD mine'" >> ~/.profile
+    echo "alias r='screen -RD mine'" >> ~/.profile
+    echo "export VISUAL=vim" >> ~/.profile
 fi
 
 if [[ ! -f ~/.vimrc || -z `grep "^:syntax on" ~/.vimrc` ]]; then
-	echo ":syntax on" >> ~/.vimrc
+    echo ":syntax on" >> ~/.vimrc
     echo "set ts=4" >> ~/.vimrc
     echo "set sts=4" >> ~/.vimrc
     echo "set sw=4" >> ~/.vimrc
