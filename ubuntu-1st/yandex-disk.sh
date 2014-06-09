9e5e4c45d088774c007e543a1c9a0f4b8590ff78
Ilya Roslyakov <uprial@gmail.com>
2014-06-09 13:06:04 +0400

    Fixed Yandex-Disk install

--- ubuntu-1st/yandex-disk.sh
+++ ubuntu-1st/yandex-disk.sh
@@ -2,11 +2,15 @@
 
 set -e
 
-echo "deb http://repo.yandex.ru/yandex-disk/deb/ stable main" \
-        | tee -a /etc/apt/sources.list.d/yandex.list > /dev/null \
-        && wget http://repo.yandex.ru/yandex-disk/YANDEX-DISK-KEY.GPG -O- \
-        | apt-key add - \
-        && sudo apt-get update \
-        && sudo apt-get install -y yandex-disk
+if [[ -z `dpkg -l | grep yandex-disk` ]]; then
+    echo "deb http://repo.yandex.ru/yandex-disk/deb/ stable main" \
+            | tee -a /etc/apt/sources.list.d/yandex.list > /dev/null \
+            && wget http://repo.yandex.ru/yandex-disk/YANDEX-DISK-KEY.GPG -O- \
+            | apt-key add - \
+            && sudo apt-get update \
+            && sudo apt-get install -y yandex-disk
 
-yandex-disk setup
+    yandex-disk setup
+else
+    echo "yandex-disk already installed"
+fi
