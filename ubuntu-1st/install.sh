3c076f8a659c1aa9acf1794858d73d696efe8153
Ilya Roslyakov <uprial@gmail.com>
2015-03-21 20:21:24 +0300

    Added Python

--- ubuntu-1st/install.sh
+++ ubuntu-1st/install.sh
@@ -6,6 +6,7 @@ set -e
 `dirname $0`/profile.sh
 `dirname $0`/yandex-disk.sh
 `dirname $0`/java.sh
+`dirname $0`/python.sh
 cp -f `dirname $0`/../g /usr/local/bin/
 
 echo 'DONE'
---
+++ ubuntu-1st/python.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+
+apt-get install -y python-pip
+pip install PyYAML
