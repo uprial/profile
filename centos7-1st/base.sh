5f35489cea5e65261806fec918d6461aaad46bd2
Ilya Roslyakov <uprial@gmail.com>
2016-01-09 22:40:37 +0300

    Added pylint to Centos-7

--- centos7-1st/base.sh
+++ centos7-1st/base.sh
@@ -7,5 +7,5 @@ yum update -y
 git config --global user.name "Ilya Roslyakov"
 git config --global user.email uprial@gmail.com
 
-yum install -y gcc ImageMagick-devel unzip screen vim
+yum install -y gcc ImageMagick-devel unzip screen vim pylint
 yum install -y bc
