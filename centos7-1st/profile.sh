88baa80add1360cdd5bd47d36b86754e2aeaa6ab
Ilya Roslyakov <uprial@gmail.com>
2016-04-04 07:20:29 +0300

    Added comments in ~/.bashrc

--- centos7-1st/profile.sh
+++ centos7-1st/profile.sh
@@ -5,6 +5,8 @@ set -e
 sed -i "s/^alias l='ls \-CF'/#alias l='ls -CF'/g" ~/.bashrc
 
 if [[ -z `grep "^alias \.\.=" ~/.bashrc` ]]; then
+    echo "" >> ~/.bashrc
+    echo "# Rial local definitions" >> ~/.bashrc
     echo "alias ..='cd ..'" >> ~/.bashrc
     echo "alias l='ls -laG'" >> ~/.bashrc
     echo "alias gd='git diff --color'" >> ~/.bashrc
