d16eee649fb626363ab8dda24cbfc9faf0103fe7
Ilya Roslyakov <uprial@gmail.com>
2016-04-06 00:23:11 +0300

    Download google files

--- centos7-1st/google-drive.sh
+++ centos7-1st/google-drive.sh
@@ -4,5 +4,10 @@ set -e
 
 `dirname $0`/gdrive.sh
 
-echo "STUB"
-exit 1
+source ~/.bashrc
+
+gdrive sync list > /dev/null
+
+parent=$(gdrive list --query "name = 'Mine'" --no-header | cut -f1 -d' ')
+gdrive download query --recursive \
+                      --path ~/Mine "'${parent}' in parents" || :
