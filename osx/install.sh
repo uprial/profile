4330520f23f50aca0cacfe919b32d026c4171288
BuildTools <unconfigured@null.spigotmc.org>
2019-05-14 15:17:49 +0300

    Added OSX profile

---
+++ osx/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+`dirname $0`/profile.sh
+sudo cp -f `dirname $0`/../g /usr/local/bin/
+
+echo 'DONE'
---
+++ osx/profile.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+sed -i '' "s/^alias l='ls \-CF'/#alias l='ls -CF'/g" ~/.profile
+
+if [[ -z `grep "^alias \.\.=" ~/.profile` ]]; then
+    echo "" >> ~/.profile
+    echo "# Rial local aliases definitions" >> ~/.profile
+    echo "alias ..='cd ..'" >> ~/.profile
+    echo "alias l='ls -laG'" >> ~/.profile
+    echo "alias gd='git diff --color'" >> ~/.profile
+    echo "alias r='screen -RD mine'" >> ~/.profile
+    echo "export VISUAL=vim" >> ~/.profile
+fi
+
+if [[ ! -f ~/.vimrc || -z `grep "^:syntax on" ~/.vimrc` ]]; then
+    echo ":syntax on" >> ~/.vimrc
+    echo "set hlsearch" >> ~/.vimrc
+    echo "set ts=4" >> ~/.vimrc
+    echo "set sts=4" >> ~/.vimrc
+    echo "set sw=4" >> ~/.vimrc
+    echo "set modeline" >> ~/.vimrc
+    echo "set et" >> ~/.vimrc
+    echo "set mouse=r" >> ~/.vimrc
+fi
+
+if [[ ! -f ~/.gitconfig || -z `grep "^[color]" ~/.gitconfig` ]]; then
+    echo "[color]" >> ~/.gitconfig
+    echo "    ui = true" >> ~/.gitconfig
+fi
