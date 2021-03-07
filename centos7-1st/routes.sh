464f5e4143ce8e8b2bf91a15fa40f83b635bb59e
Ilya Roslyakov <uprial@gmail.com>
2021-03-07 12:34:53 +0300

    Fixed routes for CentOS7

--- centos7-1st/routes.sh
+++ centos7-1st/routes.sh
@@ -2,8 +2,14 @@
 
 set -e
 
-systemctl enable firewalld
-systemctl start firewalld
 
-firewall-cmd --permanent --add-port=25565/tcp
-firewall-cmd --permanent --add-port=80/tcp
+iptables -I INPUT -p tcp --dport 25565 -j ACCEPT
+iptables -I INPUT -p tcp --dport 80 -j ACCEPT
+
+# CentOS 8 only
+
+#systemctl enable firewalld
+#systemctl start firewalld
+
+#firewall-cmd --permanent --add-port=25565/tcp
+#firewall-cmd --permanent --add-port=80/tcp
