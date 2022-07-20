084ca402bf0f7b4b558b13b055c24383f67a09dc
Ilya Roslyakov <uprial@gmail.com>
2022-07-21 00:05:27 +0300

    Fixed yandex disk for Ubuntu-20

--- ubuntu-1st/yandex-disk.sh
+++ ubuntu-1st/yandex-disk.sh
@@ -7,11 +7,11 @@ if [[ -z `dpkg -l | grep yandex-disk` ]]; then
             | tee -a /etc/apt/sources.list.d/yandex.list > /dev/null \
             && wget http://repo.yandex.ru/yandex-disk/YANDEX-DISK-KEY.GPG -O- \
             | apt-key add - \
-            && sudo apt-get update -y \
-            && sudo apt-get install -y yandex-disk
+            && apt update -y \
+            && apt install -y yandex-disk
 
     yandex-disk setup
-    service yandex-disk status
+    yandex-disk status
 else
     echo "yandex-disk already installed"
 fi
